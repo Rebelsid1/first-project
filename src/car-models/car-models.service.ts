f8590a80f46dde79e0790a4499af8d3f2c734e25
Siddharth Sethi <siddharth.sethi@toyota.com>
2023-10-11 21:25:55 +1000

    bug fixing

--- src/car-models/car-models.service.ts
+++ src/car-models/car-models.service.ts
@@ -13,4 +13,27 @@ export class CarModelsService {
       Quattro: true,
     },
   ];
+  findAll() {
+    return this.carModels;
+  }
+  findOne(id: string) {
+    return this.carModels.find((item) => item.id === +id);
+  }
+
+  create(createcarModelsDto: any) {
+    this.carModels.push(createcarModelsDto);
+  }
+
+  update(id: string, updateCarModelsDto: any) {
+    const existingModel = this.findOne(id);
+    if (existingModel) {
+    }
+  }
+
+  remove(id: string) {
+    const carModelIndex = this.carModels.findIndex((item) => item.id);
+    if (carModelIndex >= 0) {
+      this.carModels.slice(carModelIndex, 1);
+    }
+  }
 }
